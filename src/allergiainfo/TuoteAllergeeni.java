3efed481b94c10a6a0897ccb52128129285381b1
laivii <91199307+laivii@users.noreply.github.com>
2025-03-08 13:33:20 +0200

    add TuoteAllergeeni and TuoteAllergeenit classes

---
+++ src/allergiainfo/TuoteAllergeeni.java
@@ -0,0 +1,98 @@
+package allergiainfo;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+/**
+ * Vastuut:
+ * - tietää tuoteAllergeenin kentät
+ * - osaa tarkistaa kentän oikeellisuuden
+ * @author Viivi
+ * @version 7.3.2025
+ *
+ */
+public class TuoteAllergeeni {
+    
+    private int tuoteId         = 0;
+    private int allergeeniId    = 0;
+    private int yksiloivaNro    = 0;
+
+    
+    /**
+     * Alustetaan TuoteAllergeeni
+     */
+    public TuoteAllergeeni() {
+        //Ei tarvita vielä mitään
+    }
+    
+    
+    /**
+     * Alustetaan tietyn tuotteen allegeeni
+     * @param tuoteId tuotteen id
+     * @param allergeeniId allergeenin id
+     */
+    public TuoteAllergeeni(int tuoteId, int allergeeniId) {
+        this.tuoteId        = tuoteId;
+        this.allergeeniId   = allergeeniId;
+    }
+    
+    
+    /**
+     * Apumetodi, jolla saadaan täytettyä testiarvot TuoteAllergeenille
+     * @param tuote tuotteen id
+     * @param allergeeni allergeenin id
+     */
+    public void taytaTuoteAllergeeniTiedoilla(int tuote, int allergeeni) {
+        this.tuoteId      = tuote;
+        this.allergeeniId = allergeeni;
+        this.yksiloivaNro = (int)(Math.random() * 9999);
+    }
+    
+    
+    /**
+     * Tulostetaan TuoteAllergeenin tiedot
+     * @param out tietovirta johon tulostetaan
+     */
+    public void tulosta( PrintStream out ) {
+        out.println(String.format( "%02d", this.tuoteId ) + " " + String.format( "%02d", this.allergeeniId ) + " " + String.format( "%03d",this.yksiloivaNro ));
+    }
+    
+    
+    /**
+     * Tulostetaan TuoteAllergeenin tiedot
+     * @param os tietovirta johon tulostetaan
+     */
+    public void tulosta(OutputStream os) {
+        tulosta(new PrintStream(os));
+    }
+    
+    
+    /**
+     * Palautetaan mille tuotteelle allergeeni kuuluu
+     * @return tuotteen id:n
+     */
+    public int haeTuoteID() {
+        return this.tuoteId;
+    }
+    
+    
+    /**
+     * Palautetaan mikä allergeeni kyseessä
+     * @return allergeenin id
+     */
+    public int haeAllergeeniID() {
+        return this.allergeeniId;
+    }
+    
+    
+    /**
+     * Testataan TuoteAllergeeni luokkaa
+     * @param args ei käytössä
+     */
+    public static void main(String[] args) {
+        TuoteAllergeeni ta = new TuoteAllergeeni();
+        ta.taytaTuoteAllergeeniTiedoilla(1,1);
+        ta.tulosta(System.out);
+    }
+
+}
---
+++ src/allergiainfo/TuoteAllergeenit.java
@@ -0,0 +1,147 @@
+package allergiainfo;
+
+import java.util.*;
+
+/**
+ * @author Viivi
+ * @version 8.3.2025
+ *
+ */
+public class TuoteAllergeenit implements Iterable<TuoteAllergeeni> {
+    
+    /**  Taulukko TuoteAllergeeneista  */
+    private final Collection<TuoteAllergeeni> alkiot = new ArrayList<TuoteAllergeeni>();
+    
+    
+    /**
+     * TuoteAllergeenien alustminen
+     */
+    public TuoteAllergeenit() {
+        // toistaiseksi ei tarvitse tehdä mitään
+    }
+    
+    
+    /**
+     * Lisätään uusi TuoteAllergeeni tietorakenteeseen. Ottaa TuoteAllergeenin omistukseensa.
+     * @param ta TuoteAllergeeni joka halutaan lisätä taulukkoon
+     */
+    public void lisaa( TuoteAllergeeni ta ) {
+        alkiot.add( ta );
+    }   
+    
+
+    /**
+     * Iteraattori kaikkien TuoteAllergeenien läpikäymiseen
+     * @return TuoteAllergeeni-iteraattori
+     * 
+     * @example
+     * <pre name="test">
+     * #PACKAGEIMPORT
+     * #import java.util.*;
+     * 
+     *  TuoteAllergeenit tat = new TuoteAllergeenit();
+     *  TuoteAllergeeni laktoosi  = new TuoteAllergeeni(1,1); tat.lisaa(laktoosi);
+     *  TuoteAllergeeni kananmuna = new TuoteAllergeeni(1,2); tat.lisaa(kananmuna);
+     *  TuoteAllergeeni pahkinat  = new TuoteAllergeeni(1,3); tat.lisaa(pahkinat);
+     *  TuoteAllergeeni selleri   = new TuoteAllergeeni(1,4); tat.lisaa(selleri);
+     *  TuoteAllergeeni sinappi   = new TuoteAllergeeni(1,5); tat.lisaa(sinappi);
+     *  
+     *  Iterator<TuoteAllergeeni> i2 = tat.iterator();
+     *  i2.next() === laktoosi;
+     *  i2.next() === kananmuna;
+     *  i2.next() === pahkinat;
+     *  i2.next() === selleri;
+     *  i2.next() === sinappi;
+     *  i2.next() === laktoosi;  #THROWS NoSuchElementException  
+     *  
+     *  int n = 0;
+     *  int jnrot[] = {1,2,3,4,5};
+     *  
+     *  for ( TuoteAllergeeni ta: tat ) { 
+     *    ta.haeAllergeeniID() === jnrot[n]; n++;  
+     *  }
+     *  
+     *  n === 5;
+     *  
+     * </pre>
+     */
+    @Override
+    public Iterator<TuoteAllergeeni> iterator() {
+        return alkiot.iterator();
+    }
+    
+    
+    /**
+     * Haetaan kaikki jäsen harrastukset
+     * @param tunnusnro jäsenen tunnusnumero jolle harrastuksia haetaan
+     * @return tietorakenne jossa viiteet löydetteyihin harrastuksiin
+     * @example
+     * <pre name="test">
+     * #import java.util.*;
+     * 
+     *  TuoteAllergeenit tat = new TuoteAllergeenit();
+     *  TuoteAllergeeni  ta1 = new TuoteAllergeeni(1,1); tat.lisaa(ta1);
+     *  TuoteAllergeeni  ta2 = new TuoteAllergeeni(1,2); tat.lisaa(ta2);
+     *  TuoteAllergeeni  ta3 = new TuoteAllergeeni(2,1); tat.lisaa(ta3);
+     *  TuoteAllergeeni  ta4 = new TuoteAllergeeni(1,3); tat.lisaa(ta4);
+     *  TuoteAllergeeni  ta5 = new TuoteAllergeeni(2,2); tat.lisaa(ta5);
+     *  TuoteAllergeeni  ta6 = new TuoteAllergeeni(3,1); tat.lisaa(ta6);
+     *  
+     *  List<TuoteAllergeeni> loytyneet;
+     *  loytyneet = tat.annaTuotteenAllergeenit(1);
+     *  loytyneet.size() === 3; 
+     *  loytyneet = tat.annaTuotteenAllergeenit(4);
+     *  loytyneet.size() === 0;
+     *  loytyneet = tat.annaTuotteenAllergeenit(2);
+     *  loytyneet.size() === 2; 
+     *  loytyneet.get(0) == ta3 === true;
+     *  loytyneet.get(1) == ta5 === true;
+     *  loytyneet = tat.annaTuotteenAllergeenit(3);
+     *  loytyneet.size() === 1; 
+     *  loytyneet.get(0) == ta6 === true;
+     * </pre> 
+     */
+    public List<TuoteAllergeeni> annaTuotteenAllergeenit( int tunnusnro ) {
+        
+        List<TuoteAllergeeni> loydetyt = new ArrayList<TuoteAllergeeni>();
+        
+        for (TuoteAllergeeni ta : alkiot)
+            if (ta.haeTuoteID() == tunnusnro) loydetyt.add(ta);
+        
+        return loydetyt;
+    }
+
+ 
+    /**
+     * @param args ei käytössä
+     */
+    public static void main(String[] args) {
+        TuoteAllergeenit tat = new TuoteAllergeenit();
+        
+        TuoteAllergeeni ta1 = new TuoteAllergeeni();
+        TuoteAllergeeni ta2 = new TuoteAllergeeni();
+        TuoteAllergeeni ta3 = new TuoteAllergeeni();
+        TuoteAllergeeni ta4 = new TuoteAllergeeni();
+        
+        ta1.taytaTuoteAllergeeniTiedoilla(1,1);
+        ta2.taytaTuoteAllergeeniTiedoilla(1,2);
+        ta3.taytaTuoteAllergeeniTiedoilla(1,3);
+        ta4.taytaTuoteAllergeeniTiedoilla(1,4);
+
+        tat.lisaa(ta1);
+        tat.lisaa(ta2);
+        tat.lisaa(ta3);
+        tat.lisaa(ta2);
+        tat.lisaa(ta4);
+
+        System.out.println("============= TuoteAllergeenit testi =================");
+
+        List<TuoteAllergeeni> tuoteAllergeenit = tat.annaTuotteenAllergeenit(1);
+
+        for (TuoteAllergeeni ta : tuoteAllergeenit) {
+            System.out.print(ta.haeAllergeeniID() + " ");
+            ta.tulosta(System.out);
+        }
+
+    }
+}
