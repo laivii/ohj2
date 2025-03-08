a973db5e9f06f939a39f8714706395bab948c172
laivii <91199307+laivii@users.noreply.github.com>
2025-03-08 13:35:43 +0200

    add haeTuoteIdlla method

--- src/allergiainfo/Tuotteet.java
+++ src/allergiainfo/Tuotteet.java
@@ -10,7 +10,7 @@ package allergiainfo;
  * Tuote-luokka
  * 
  * @author Viivi
- * @version 6.3.2025
+ * @version 8.3.2025
  */
 public class Tuotteet {
     
@@ -35,19 +35,25 @@ public class Tuotteet {
      * @example
      * <pre name="test">
      *  #THROWS SailoException
+     *  
      *  Tuotteet tuotteet = new Tuotteet();
+     *  
      *  Tuote t = new Tuote();
      *  Tuote t2 = new Tuote();
+     *  
      *  tuotteet.haeLkm() === 0;
+     *  
      *  tuotteet.lisaa(t);  tuotteet.haeLkm() === 1;
      *  tuotteet.lisaa(t2); tuotteet.haeLkm() === 2;
      *  tuotteet.lisaa(t);  tuotteet.haeLkm() === 3;
+     *  
      *  tuotteet.anna(0) === t;
      *  tuotteet.anna(1) === t2;
      *  tuotteet.anna(2) === t;
      *  tuotteet.anna(1) == t   === false;
      *  tuotteet.anna(1) == t2  === true;
      *  tuotteet.anna(3) === t; #THROWS IndexOutOfBoundsException
+     *  
      *  tuotteet.lisaa(t);  tuotteet.haeLkm() === 4;
      *  tuotteet.lisaa(t2); tuotteet.haeLkm() === 5;
      *  tuotteet.lisaa(t); #THROWS SailoException
@@ -70,9 +76,9 @@ public class Tuotteet {
      *   Tuote t2 = new Tuote();
      *   Tuote t3 = new Tuote();
      *   
-     *   t1.rekisteroi(); t1.getId() === 1;
-     *   t2.rekisteroi(); t2.getId() === 2;
-     *   t3.rekisteroi(); t3.getId() === 3;
+     *   t1.rekisteroi(); t1.haeId() === 1;
+     *   t2.rekisteroi(); t2.haeId() === 2;
+     *   t3.rekisteroi(); t3.haeId() === 3;
      *   
      *   tuotteet.lisaa(t1); tuotteet.haeLkm() === 1;
      *   tuotteet.lisaa(t2); tuotteet.haeLkm() === 2;
@@ -109,18 +115,18 @@ public class Tuotteet {
      *   Tuote t2 = new Tuote();
      *   Tuote t3 = new Tuote();
      *   
-     *   t1.rekisteroi(); t1.getId() === 4;
-     *   t2.rekisteroi(); t2.getId() === 5;
-     *   t3.rekisteroi(); t3.getId() === 6;
+     *   t1.rekisteroi(); t1.haeId() === 7;
+     *   t2.rekisteroi(); t2.haeId() === 8;
+     *   t3.rekisteroi(); t3.haeId() === 9;
      *   
      *   tuotteet.lisaa(t1); tuotteet.haeLkm() === 1;
      *   tuotteet.lisaa(t2); tuotteet.haeLkm() === 2;
      *   tuotteet.lisaa(t3); tuotteet.haeLkm() === 3;
      *   
-     *   tuotteet.poistaTietty(4); tuotteet.haeLkm() === 2;
-     *   tuotteet.poistaTietty(7); tuotteet.haeLkm() === 2;
-     *   tuotteet.poistaTietty(5); tuotteet.haeLkm() === 1;
-     *   tuotteet.poistaTietty(6); tuotteet.haeLkm() === 0;
+     *   tuotteet.poistaTietty(7);  tuotteet.haeLkm() === 2;
+     *   tuotteet.poistaTietty(99); tuotteet.haeLkm() === 2;
+     *   tuotteet.poistaTietty(8);  tuotteet.haeLkm() === 1;
+     *   tuotteet.poistaTietty(9);  tuotteet.haeLkm() === 0;
      * </pre>
      */
     public void poistaTietty(int id) {
@@ -147,6 +153,50 @@ public class Tuotteet {
         
         this.alkiot = uusiLista;
         this.lkm -= 1;
+    }
+    
+    
+    /**
+     * Palauttaa tietyn tuotteen id:n perusteella
+     * @param id tuotteen id
+     * @return id:n perusteella rajatun tuotteen
+     * @throws SailoException jos tuotetta ei löydy annetulla id:llä
+     * @example
+     * <pre name="test">
+     * #THROWS SailoException
+     *  Tuotteet tuotteet = new Tuotteet();
+     *  
+     *  Tuote t1 = new Tuote();
+     *  Tuote t2 = new Tuote();
+     *  Tuote t3 = new Tuote();
+     *  
+     *  t1.rekisteroi(); t1.haeId() === 4;
+     *  t2.rekisteroi(); t2.haeId() === 5;
+     *  t3.rekisteroi(); t3.haeId() === 6;
+     *  
+     *  tuotteet.lisaa(t1);
+     *  tuotteet.lisaa(t2);
+     *  tuotteet.lisaa(t3);
+     *  
+     *  tuotteet.haeTuoteIdlla(4) === t1;
+     *  tuotteet.haeTuoteIdlla(6) === t3;
+     *  tuotteet.haeTuoteIdlla(5) === t2;
+     *  tuotteet.haeTuoteIdlla(55); #THROWS SailoException
+     * </pre>
+     */
+    public Tuote haeTuoteIdlla( int id ) throws SailoException {
+        Tuote tuote = null;
+        
+        for(Tuote t: this.alkiot ) {
+            if(t != null && t.haeId() == id ) {
+                tuote = t;
+                break;
+            }
+        }
+        
+        if( tuote == null ) throw new SailoException("Tuotetta ei löytynyt id:llä " + id );
+        
+        return tuote;
     }
     
  
