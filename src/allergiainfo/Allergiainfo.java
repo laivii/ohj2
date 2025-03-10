c05beb4d62be39db68e6abfbb43d76204bd98fdd
laivii <91199307+laivii@users.noreply.github.com>
2025-03-10 18:15:12 +0200

    update 'lisaa' method names, add alustaAllergeenit method

--- src/allergiainfo/Allergiainfo.java
+++ src/allergiainfo/Allergiainfo.java
@@ -11,6 +11,7 @@ public class Allergiainfo {
     private Tuotteet         tuotteet         = new Tuotteet();
     private Allergeenit      allergeenit      = new Allergeenit();
     private TuoteAllergeenit tuoteAllergeenit = new TuoteAllergeenit();
+    private Ravintolat       ravintolat       = new Ravintolat();
     
     
     /**
@@ -19,7 +20,7 @@ public class Allergiainfo {
      * @throws SailoException jos lisääminen ei onnistu
      * 
      */
-    public void lisaaTuote(Tuote tuote) throws SailoException {
+    public void lisaa(Tuote tuote) throws SailoException {
         this.tuotteet.lisaa(tuote);
     }
     
@@ -28,8 +29,24 @@ public class Allergiainfo {
      * @param allergeeni joka halutaan lisätä
      * @throws SailoException jos lisääminen ei onnistu
      */
-    public void lisaaAllergeeni(Allergeeni allergeeni ) throws SailoException {
+    public void lisaa(Allergeeni allergeeni ) throws SailoException {
         this.allergeenit.lisaa(allergeeni);
+    }
+    
+    /**
+     * Lisätään uusi tuoteAllergeeni eli yhdistetään allergeeni tuotteeseen
+     * @param tuoteID tuotteen id
+     * @param allergeeniID allergeenin id
+     */
+    public void lisaa(int tuoteID, int allergeeniID ) {
+        /*
+         * TODO muokkaa järkeväksi
+         * TuoteAllergeeni ta = new TuoteAllergeeni( tuoteID, allergeeniID );
+         * this.tuoteAllergeeni.lisaa( ta );
+         */
+        TuoteAllergeeni ta = new TuoteAllergeeni();
+        ta.taytaTuoteAllergeeniTiedoilla( tuoteID, allergeeniID );
+        this.tuoteAllergeenit.lisaa( ta );
     }
     
     
@@ -68,6 +85,25 @@ public class Allergiainfo {
     
     
     /**
+     * Palauttaa ravintoloiden määrän
+     * @return ravintoloiden lukumäärän
+     */
+    public int haeRavintoloita() {
+        return this.ravintolat.haeLkm();
+    }
+    
+    
+    /**
+     * Palauttaa listana tuotteen allergeenit
+     * @param id tuotteen id
+     * @return lista tuotteen allergeeneista
+     */
+    public List<TuoteAllergeeni> haeTuotteenAllergeenit(int id) {
+        return this.tuoteAllergeenit.annaTuotteenAllergeenit( id );
+    }
+    
+    
+    /**
      * Palauttaa tuotteen id:n perusteella
      * @param id tuotteen id
      * @return Tuote
@@ -90,76 +126,120 @@ public class Allergiainfo {
     
     
     /**
-     * Palauttaa tietyn tuotteen id:n perusteella
-     * @param id monesko tuote
+     * Palauttaa ravintolan id:n perusteella
+     * @param id ravintolan id
+     * @return ravintolan
+     * @throws SailoException jos annetulla id:lla ei löydy ravintolaa
+     */
+    public Ravintola haeRavintolaIdlla( int id ) throws SailoException {
+        return this.ravintolat.haeRavintolaIdlla( id );
+    }
+    
+    
+    /**
+     * Palauttaa tietyn tuotteen paikan perusteella
+     * @param i monesko tuote
      * @return tuote paikasta i
      */
-    public Tuote annaTuote(int id) {
-        return this.tuotteet.anna(id);
+    public Tuote annaTuote(int i) {
+        return this.tuotteet.anna(i);
     }
     
     
     /**
-     * Palauttaa allergeenin id:n perusteella
-     * @param id allergeenin id
+     * Palauttaa allergeenin paikan perusteella
+     * @param i monesko allergeeni
      * @return allergeeni paikasta i
      */
-    public Allergeeni annaAllergeeni(int id) {
-        return this.allergeenit.anna(id);
+    public Allergeeni annaAllergeeni(int i) {
+        return this.allergeenit.anna(i);
     }
     
     
     /**
-     * Palauttaa listan tuotteen allergeeneista
-     * @param id tuotteen id
-     * @return palauttaa tuotteen allergeenit listana
+     * Palauttaa i:n ravintolan
+     * @param i monesko ravintola
+     * @return ravintola paikasta i
      */
-    public List<TuoteAllergeeni> annaTuotteenAllergeenit(int id ) {
-        return this.tuoteAllergeenit.annaTuotteenAllergeenit(id);
+    public Ravintola annaRavintola(int i ) {
+        return this.ravintolat.anna(i);
     }
     
     
     /**
+     * Alustetaan ravintolat
+     */
+    public void alustaRavintolat() {
+        this.ravintolat.taytaRavintolatTiedoilla(this.ravintolat);
+    }
+    
+    
+    /**
+     * Alustetaan allergeenit
+     */
+    public void alustaAllergeenit() {
+        this.allergeenit.taytaAllergeenitTiedoilla(this.allergeenit);
+    }
+    
+    
+    /**
+     * Testataan Allergiainfo luokkaa
      * @param args ei käytössä
      */
     public static void main(String[] args) {
         Allergiainfo ai = new Allergiainfo();
         
+        System.out.println("Allergiainfon ravintolat: ");
+        
+        ai.alustaRavintolat();
+        
+        for( int i = 0; i < ai.haeRavintoloita(); i++ ) {
+            Ravintola r = ai.annaRavintola(i);
+            r.tulosta(System.out);
+        }
+     
         Tuote t = new Tuote();
-        Tuote t2 = new Tuote();
-        Tuote t3 = new Tuote();
-        Tuote t4 = new Tuote();
         
         t.rekisteroi();
         t.taytaTuoteTiedoilla();
-        t2.rekisteroi();
-        t2.taytaTuoteTiedoilla();
-        t3.rekisteroi();
-        t3.taytaTuoteTiedoilla();
-        t4.rekisteroi();
-        t4.taytaTuoteTiedoilla();
         
         try {
-            ai.lisaaTuote(t);
-            ai.lisaaTuote(t2);
-            ai.lisaaTuote(t3);
-            ai.lisaaTuote(t4);
+            ai.lisaa(t);
         } catch (SailoException e) {
             System.err.println(e.getMessage());
         }
         
-        for( int i = 0; i < ai.haeTuotteita(); i++) {
-            Tuote tuote = ai.annaTuote(i);
-            tuote.tulosta(System.out);
+        System.out.println("\nAllergiainfon tuotteet:");
+        t.tulosta(System.out);
+        
+        ai.alustaAllergeenit();
+        
+        System.out.println("\nAllergiainfon allergeenit:");
+        for( int i = 0; i < ai.haeAllergeeneja(); i++ ) {
+            Allergeeni a = ai.annaAllergeeni(i);
+            a.tulosta(System.out);
         }
         
-        ai.poistaTiettyTuote(1);
+        ai.lisaa(1, 1);
+        ai.lisaa(1, 2);
+        ai.lisaa(1, 3);
+        ai.lisaa(1, 4);
         
-        System.out.println("Poistettu ensimmäinen");
+        List<TuoteAllergeeni> tuotteenAllergeenit = ai.haeTuotteenAllergeenit(1);
         
-        for( int i = 0; i < ai.haeTuotteita(); i++) {
-            Tuote tuote = ai.annaTuote(i);
-            tuote.tulosta(System.out);
+        System.out.println("\nTuotteen allergeenit:");
+        for(TuoteAllergeeni ta: tuotteenAllergeenit ) {
+            ta.tulosta(System.out);
         }
+        
+        
+//        ai.poistaTiettyTuote(1);
+//        
+//        System.out.println("Poistettu ensimmäinen");
+//        
+//        for( int i = 0; i < ai.haeTuotteita(); i++) {
+//            Tuote tuote = ai.annaTuote(i);
+//            tuote.tulosta(System.out);
+//        }
     }
 }
