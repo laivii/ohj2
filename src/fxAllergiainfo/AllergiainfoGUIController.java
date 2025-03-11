18620aa4a56c5c246a6960678c5630ca11cd6819
laivii <91199307+laivii@users.noreply.github.com>
2025-03-11 14:02:16 +0200

    update AllergiainfoGUIController, refactor/update code, add methods, update existing ones

--- src/fxAllergiainfo/AllergiainfoGUIController.java
+++ src/fxAllergiainfo/AllergiainfoGUIController.java
@@ -2,12 +2,17 @@ package fxAllergiainfo;
 
 import java.net.URL;
 import java.io.PrintStream;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import allergiainfo.Allergeeni;
 import allergiainfo.Allergiainfo;
+import allergiainfo.Ravintola;
 import allergiainfo.SailoException;
 import allergiainfo.Tuote;
+import allergiainfo.TuoteAllergeeni;
 import fi.jyu.mit.fxgui.Dialogs;
+import fi.jyu.mit.fxgui.ListChooser;
 import fi.jyu.mit.fxgui.ModalController;
 import fi.jyu.mit.fxgui.TextAreaOutputStream;
 import javafx.fxml.FXML;
@@ -22,14 +27,21 @@ import javafx.scene.text.Font;
 
 /**
  * @author Viivi
- * @version 6.3.2025
+ * @version 11.3.2025
  */
 public class AllergiainfoGUIController implements Initializable {
     
     @FXML private TextField searchField;
-    @FXML private ScrollPane panelTuotteet;
+    @FXML private ScrollPane panelTuote;
+    @FXML private ListChooser<Tuote> chooserTuotteet;
     //@FXML private ComboBox<?> searchTermCB; //TODO add arguments to CB
   	
+    
+    @Override
+    public void initialize(URL url, ResourceBundle bundle) {
+        alusta();
+    }
+    
     
 	@FXML
 	private void avaaTuotteidenhallinta() {
@@ -44,21 +56,19 @@ public class AllergiainfoGUIController implements Initializable {
 	
 	
 	@FXML
-	private void poistaViimeinenTuote() {
-	    poistaViimeinen();
+	private void lisaaAllergeeniValitulle() {
+	    lisaaTuoteAllergeeni();
 	}
-	
-	@Override
-    public void initialize(URL url, ResourceBundle bundle) {
-        alusta();
-    }
     
     
 //============================================================================================================
 // Tästä eteenpäin ei käyttöliittymään suoraan liittyvää koodia
+
+
+    private Allergiainfo allergiainfo;
+	private TextArea areaTuote = new TextArea();
+	private Tuote tuoteKohdalla;
 	
-	private Allergiainfo allergiainfo;
-	private TextArea areaTuotteet = new TextArea();
 	
 	
 	/**
@@ -74,10 +84,45 @@ public class AllergiainfoGUIController implements Initializable {
 	 * Alustetaan tekstialue
 	 */
 	private void alusta() {
-	    panelTuotteet.setContent(areaTuotteet);
-	    areaTuotteet.setFont(new Font("Courier New", 12));
-	    areaTuotteet.setEditable(false);
-	    panelTuotteet.setFitToHeight(true);
+	    panelTuote.setContent(areaTuote);
+	    areaTuote.setFont(new Font("Courier New", 12));
+	    areaTuote.setEditable(false);
+	    panelTuote.setFitToHeight(true);
+	    
+	    chooserTuotteet.clear();
+	    chooserTuotteet.addSelectionListener( e -> naytaTuote());
+	}
+	
+	
+	/**
+	 * Näyttää listasta valitun tuotteen tiedot, tilapäisesti yhteen isoon edit-kenttään
+	 */
+	private void naytaTuote() {
+	    tuoteKohdalla = chooserTuotteet.getSelectedObject();
+	    
+	    if( tuoteKohdalla == null ) return;
+	    
+	    areaTuote.setText("");
+	    try( PrintStream os = TextAreaOutputStream.getTextPrintStream(areaTuote)){
+	        tulosta(os, tuoteKohdalla);
+	    }
+	}
+	
+	/**
+	 * Hakee tuotteiden tiedot listaan
+	 * @param tuoteID haettavan tuotteen id
+	 */
+	private void hae( int tuoteID ) {
+	    chooserTuotteet.clear();
+	    
+	    int index = 0;
+	    for( int i = 0; i < this.allergiainfo.haeTuotteita(); i++ ) {
+	        Tuote tuote = this.allergiainfo.annaTuote( i );
+	        if( tuote.haeId() == tuoteID ) index = i;
+	        chooserTuotteet.add( tuote.haeNimi(), tuote);
+	    }
+	    
+	    chooserTuotteet.setSelectedIndex( index );
 	}
 	
 	
@@ -96,46 +141,64 @@ public class AllergiainfoGUIController implements Initializable {
             return;
         }
 	    
-	    areaTuotteet.clear();
-	    tulostaTuotteet(areaTuotteet);
+	    hae(tuote.haeId());
 	}
 	
-	
+
 	/**
-	 * Poistetaan tuotelistan ensimmäinen alkio
+	 * Lisätään valitulle tuotteelle allergeeni
 	 */
-	private void poistaViimeinen() {
-	    if( allergiainfo.haeTuotteita() <= 0 ) return;
-	    
-	    allergiainfo.poistaViimeinenTuote();
-	        
-	    areaTuotteet.clear();
-	    tulostaTuotteet(areaTuotteet);
-	}
-	
-	
-	/**
-	 * Tulostetaan tuotteen tiedot
-	 * @param os tietovirta johon tulostetaan
-	 * @param tuote joka halutaan tulostaa
-	 */
-	private void tulostaTuote( PrintStream os, final Tuote tuote ) {
-	    os.println("------------------------------");
-	    tuote.tulosta(os);
-	    os.println("------------------------------");
-	}
-	
-	
-	/**
-	 * Tulostaa listassa olevat tuotteet tekstialueeseen
-	 * @param text alue johon tulostetaan
-	 */
-	private void tulostaTuotteet(TextArea text) {	    
-	    try( PrintStream os = TextAreaOutputStream.getTextPrintStream(text)) {
-	        for( int i = 0; i < allergiainfo.haeTuotteita(); i++ ) {
-	            Tuote tuote = allergiainfo.annaTuote( i );
-	            tulostaTuote(os, tuote);
-	        }
-	    }
-	}
+    private void lisaaTuoteAllergeeni() {
+        /*
+         * allergeeneja 15 eri, joten demossa voidaan valita random 1-15
+         * TODO katso ettei allergeeni ole jo yhdistetty tuotteeseen
+         */
+        int allergeeniID = (int)(Math.random()* 15);
+        int tuoteID = tuoteKohdalla.haeId();
+        
+        allergiainfo.lisaa(tuoteID, allergeeniID);
+        
+        naytaTuote();
+    }
+    
+    
+    /**
+     * Tulostaa tuotteen tiedot (nimi ja allergeenit)
+     * @param os tietovirta johon tulostetaan
+     * @param tuote tulostettava tuote
+     */
+    public void tulosta(PrintStream os, final Tuote tuote) {
+        Ravintola r = null;
+        try {
+            r = allergiainfo.haeRavintolaIdlla(tuote.haeRavintolaId());
+        } catch (SailoException e) {
+            System.err.println(e.getMessage());
+        }
+ 
+        os.println("----------------------------------------------");
+        
+        tuote.tulosta(os);
+        if( r != null)
+            r.tulosta(os);
+        
+        os.println("----------------------------------------------");
+        os.println("Allergeenit:\n");
+        
+        List<TuoteAllergeeni> tat = allergiainfo.haeTuotteenAllergeenit(tuote.haeId());   
+        for (TuoteAllergeeni ta: tat) {
+            ta.tulosta(os);
+            Allergeeni a = null;
+            try {
+                a = allergiainfo.haeAllergeeniIdlla(ta.haeAllergeeniID());
+            } catch (SailoException e) {
+                System.err.println(e.getMessage());
+            }
+            
+            if( a != null )
+                a.tulosta(os);
+                os.println();
+        }
+        os.println("----------------------------------------------");
+   }
+
 }
